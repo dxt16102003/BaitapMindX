8cf534112ac398e814498b0d016f4113ff70b55a
dxt16102003 <59783789+dxt16102003@users.noreply.github.com>
2022-04-24 15:52:48 +0700

    Add files via upload

---
+++ appbai1buoi1.js
@@ -0,0 +1,5 @@
+import maytinh, {sum, calculateRectArea, calculateCircleArea} from './calculatorbai1buoi1.js';
+sum(10, 5);
+calculateRectArea(10,5);
+calculateCircleArea(30);
+maytinh({name: "dell1"})
